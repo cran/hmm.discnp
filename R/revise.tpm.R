a30e10a24154d998d45c7b9e6baf932a16ae529d
Rolf Turner <r.turner@auckland.ac.nz>
2008-07-03 00:00:00 +0000

    version 0.0-8

--- R/revise.tpm.R
+++ R/revise.tpm.R
@@ -1,4 +1,4 @@
 revise.tpm <- function(xi,mixture) {
-	if(mixture)  matrix(apply(xi,2,sum)/sum(xi),byrow=T,nrow=nrow(xi))
+	if(mixture)  matrix(apply(xi,2,sum)/sum(xi),byrow=TRUE,nrow=nrow(xi))
 	else xi/apply(xi,1,sum)
 }
