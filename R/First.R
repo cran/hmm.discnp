2e7d7c1ef5c3c945177d43b2c071427d69f40d1d
Rolf Turner <r.turner@auckland.ac.nz>
2011-11-04 00:00:00 +0000

    version 0.1-5

--- R/First.R
+++ R/First.R
@@ -1,10 +1,12 @@
-.First.lib <- function(lib,pkg) {
+.onLoad <- function(lib, pkg) {
 	library.dynam("hmm.discnp", pkg, lib)
 	ver <- read.dcf(file.path(lib, pkg, "DESCRIPTION"), "Version")
-	cat(paste(pkg, ver, "\n\n"))
-	cat("PLEASE NOTE:  The package has changed substantially\n")
-	cat("from the 0.0-x versions.  New functions have been\n")
-	cat("added and both the argument lists and the returned\n")
-	cat("values from old functions have new forms.  Please\n")
-	cat("read the ChangeLog and the documentation.\n\n")
+        packageStartupMessage(paste(pkg, ver))
+	packageStartupMessage(
+            paste("\n     PLEASE NOTE:  The package has changed substantially",
+                  "\n     from the 0.0-x versions.  New functions have been",
+                  "\n     added and both the argument lists and the returned",
+                  "\n     values from old functions have new forms.  Please",
+                  "\n     read the ChangeLog and the documentation.\n")
+        )
 }
