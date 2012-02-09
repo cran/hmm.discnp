b0aab7d2b9f58976d941325152881b1bf9341042
Rolf Turner <r.turner@auckland.ac.nz>
2012-02-09 00:00:00 +0000

    version 0.1-7

--- R/check.yval.R
+++ R/check.yval.R
@@ -3,15 +3,15 @@ yval  <- unique(unlist(y))
 fname <- as.character(sys.call(-1))[1]
 if(is.na(fname)) fname <- "call from the command line"
 if(is.null(row.names(Rho))) {
-	if(!all(yval%in%(1:nrow(Rho))))
-		stop(paste("In ",fname,
-                           ".  The values of \"y\" must be in \"1:nrow(Rho)\".\n",
-                           sep=""),call.=FALSE)
+	yval <- as.numeric(yval)
+	OK   <- all(yval%in%(1:nrow(Rho)))
+	if(!OK) stop(paste("In ",fname,".  The values of \"y\" must be in ",
+                     "\"1:nrow(Rho)\".\n",sep=""),call.=FALSE)
 } else {
-	if(!all(yval%in%(row.names(Rho))))
-		stop(paste("In ",fname,
-                           ".  The values of \"y\" must be in \"row.names(Rho)\".\n",
-                           sep=""),call.=FALSE)
+	yval <- as.character(yval)
+	OK   <- all(yval%in%row.names(Rho))
+	if(!OK) stop(paste("In ",fname,".  The values of \"y\" must be in ",
+                     "\"row.names(Rho)\".\n",sep=""),call.=FALSE)
 }
 return(invisible())
 }
